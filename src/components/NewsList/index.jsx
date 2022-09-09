00aecb1492e3242345c7a13ba40b4a1c52383619
khalilannbiya <syeichkhalil@gmail.com>
2022-09-10 06:31:22 +0700

    feat: create news card component, then integrated in card list component

--- src/components/NewsList/index.jsx
+++ src/components/NewsList/index.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./NewsList.module.css";
-
+import { NewsCard } from "../";
 const NewsList = ({ articles }) => {
    return (
       <div className={styles.newsList}>
          {articles.map((article, index, arr) => {
             return (
-               <div key={index}>
-                  <h1>{article.title}</h1>
-               </div>
+               <NewsCard
+                  key={index}
+                  src={article.urlToImage}
+                  title={article.title}
+                  publishedAt={article.publishedAt}
+                  author={article.author}
+                  sourceName={article.source.name}
+                  description={article.description}
+                  url={article.url}
+                  notLastChild={!(arr.length === index + 1)}
+               />
             );
          })}
       </div>
